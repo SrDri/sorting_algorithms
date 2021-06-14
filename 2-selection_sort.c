1b889b2cd0ef0ff211e530295481cff34ff9936d
SrDri <juanjcarabali@gmail.com>
2021-06-14 07:40:46 +0700

    Task #2

---
+++ 2-selection_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * selection_sort - selection_sort algorithms
+ * @array: array sorted
+ * @size: the size array
+*/
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	size_t num_min = 0;
+	size_t siguien;
+	int aux;
+
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		num_min = i;
+		siguien = i + 1;
+		j = siguien;
+		for (j; j < size; j++)
+		{
+			if (array[num_min] > array[j])
+			{
+				num_min = j;
+			}
+		}
+		aux = array[i];
+		array[i] = array[num_min];
+		array[num_min] = aux;
+		print_array(array, size);
+	}
+}
